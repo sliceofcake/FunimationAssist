f941b81e818a0f700d1221f042baeec6573fb0b4
sliceofcake <sliceofcake@users.noreply.github.com>
2017-06-25 23:46:51 -0700

    Added ability to change color details of video.

--- README.md
+++ README.md
@@ -1,15 +1,20 @@
 # FunimationAssist  
-Speed up Funimation videos.  
+Speed up Funimation videos, change brightness/contrast/saturation.  
   
-Run this JavaScript in your browser when viewing a Funimation video player page to add the ability to speed up the anime videos. Browsers currently respect the range of 0.5x to 4x, so this tool enforces that range. The interface is a slider.  
+Run this JavaScript in your browser when viewing a Funimation video player page to add the ability to speed up anime videos. Browsers currently respect the range of 0.5x to 4x, so this tool enforces that range. The interface is a slider.  
+  
+You also have the option of adjusting color settings. The usual workflow there is to raise some values and lower others.  
   
 You can also create a bookmark with the URL/link being the funimationAssistBookmarklet.txt text. Then, whenever you click on the bookmark, it will run the script. Bookmarks that run JavaScript instead of going to a webpage are called bookmarklets.  
   
 Use case  
 Sometimes, anime series are too slow. Instead of wasting time trudging through them, just use this tool to speed them up a little bit.  
   
+Some Funimation anime series seem to have color issues. Videos may appear too dark/color-similar, or too bright and washed out. Use this tool to perform enough corrections to make such series more watchable.  
+  
 Why this works  
 Recently [end of 2016, beginning of 2017], Funimation decided to redo their website and, for whatever reason, decided to use an HTML5 video player instead of their traditional Flash video player. This allows for customer scripting - abilities that were previous made difficult because the video was sitting behind Flash.  
   
 Everything that could go wrong, within reason  
 • Funimation could change the way they set up their video element, breaking this script  
+• You could be using an old web browser, one that doesn't support the JavaScript code that I wrote  
--- funimationAssist.js
+++ funimationAssist.js
@@ -1,54 +1,82 @@
 (function(){
 	var p = {
-		elVideo : document.querySelector("#player").contentDocument.querySelector("video"),
-		elDisplay : null,
+		elVideo      : document.querySelector("#player").contentDocument.querySelector("video"),
+		controlEO : {
+			playbackRate : {elControl : null,elDisplay : null,elReset : null,reset : 1,v : 1,min:0.5,max:4,step:0.05,},
+			brightness   : {elControl : null,elDisplay : null,elReset : null,reset : 1,v : 1,min:0  ,max:4,step:0.05,},
+			contrast     : {elControl : null,elDisplay : null,elReset : null,reset : 1,v : 1,min:0  ,max:4,step:0.05,},
+			saturate     : {elControl : null,elDisplay : null,elReset : null,reset : 1,v : 1,min:0  ,max:4,step:0.05,},
+		},
+		genFilter : function(){
+			return "brightness("+this.controlEO.brightness.v+") contrast("+(this.controlEO.contrast.v*100)+"%) saturate("+(this.controlEO.saturate.v*100)+"%)";},
+		set : function(o={}){this.ooaw(o,{whatS:null,valueM:null});
+			var controlE = this.controlEO[o.whatS];
+			controlE.v = o.valueM;
+			this.pipe(o);},
+		pipe : function(o={}){this.ooaw(o,{whatS:null});
+			var controlE = this.controlEO[o.whatS];
+			switch (o.whatS){default:;
+				break;case "playbackRate":
+					this.elVideo.playbackRate = controlE.v;
+					controlE.elDisplay.textContent = "playback rate : "+controlE.v+"x";
+					controlE.elInput.value = controlE.v;
+				break;case "brightness":
+					this.elVideo.style.filter = this.genFilter();
+					controlE.elDisplay.textContent = "brightness : "+controlE.v+"x";
+					controlE.elInput.value = controlE.v;
+				break;case "contrast":
+					this.elVideo.style.filter = this.genFilter();
+					controlE.elDisplay.textContent = "contrast : "+controlE.v+"x";
+					controlE.elInput.value = controlE.v;
+				break;case "saturate":
+					this.elVideo.style.filter = this.genFilter();
+					controlE.elDisplay.textContent = "saturation : "+controlE.v+"x";
+					controlE.elInput.value = controlE.v;}},
+		
+		genElBase    : function(o={}){this.ooaw(o,{typeS:"div",classS:null,elParent:null});
+			var el = document.createElement(o.typeS);
+			if (o.classS   !== null){el.classList.add(o.classS);}
+			if (o.elParent !== null){o.elParent.appendChild(el);}
+			return el;},
+		// object <-- object ; property absorb (strong)
+		ooas:function(o,oo){
+			for (var k of Object.keys(oo)){
+				o[k] = oo[k];}
+			return o;},
+		// object <-- object ; property absorb (weak)
+		ooaw:function(o,oo){
+			for (var k of Object.keys(oo)){
+				if (typeof o[k] === "undefined"){
+					o[k] = oo[k];}}
+			return o;},
 	};
 	
 	//----
 	
-	var elMain = document.createElement("div");
-	elMain.style.position = "fixed";
-	elMain.style.bottom = "0px";
-	elMain.style.left = "0px";
-	elMain.style.width = "100%";
-	elMain.style.color = "hsla(0,0%,100%,1)";
-	elMain.style.backgroundColor = "hsla(0,0%,0%,0.8)";
-	elMain.style.padding = "4px 8px";
-	elMain.style.zIndex = "999999999";
+	var elCSS = p.genElBase({typeS:"style",elParent:document.head});
+	elCSS.textContent
+		= ".funimationAssist-main    {position:fixed;bottom:0px;left:0px;width:100%;color:hsla(0,0%,100%,1);background-color:hsla(0,0%,0%,0.8);padding:4px 4px;z-index:999999999;}"
+		+ ".funimationAssist-version {position:absolute;bottom:4px;right:20px;color:hsla(0,0%,100%,0.5);text-align:right;}"
+		+ ".funimationAssist-row     {}"
+		+ ".funimationAssist-display {display:inline-block;width:200px;background-color:hsla(0,0%,0%,1);border:1px solid white;padding:0px 4px;margin:0px 4px;text-align:right;}"
+		+ ".funimationAssist-input   {width:300px;background-color:hsla(0,0%,0%,1);}";
 	
-	var elInput = document.createElement("input");
-	elInput.style.width = "300px";
-	elInput.style.backgroundColor = "hsla(0,0%,0%,1)";
-	elInput.type  = "range";
-	elInput.min   = 0.5;
-	elInput.max   = 4;
-	elInput.step  = 0.05;
-	elInput.value = 1;
-	elInput.addEventListener("input",(function(p){return function(event){
-		p.elVideo.playbackRate = this.value;
-		p.elDisplay.textContent = this.value+"x";
-	};})(p));
+	var elMain = p.genElBase({typeS:"div",classS:"funimationAssist-main",elParent:document.body});
 	
-	var elDisplay = document.createElement("block");
-	elDisplay.style.width = "100px";
-	elDisplay.style.backgroundColor = "hsla(0,0%,0%,1)";
-	elDisplay.style.border = "1px solid white";
-	elDisplay.style.padding = "0px 4px";
-	elDisplay.style.margin = "0px 4px";
-	elDisplay.textContent = "1x";
-	p.elDisplay = elDisplay;
+	var elVersion = p.genElBase({typeS:"div",classS:"funimationAssist-version",elParent:elMain});
+	elVersion.textContent = "ver2";
 	
-	var elVersion = document.createElement("div");
-	elVersion.style.position = "absolute";
-	elVersion.style.bottom = "4px";
-	elVersion.style.right = "20px"; // padding of parent plus actual value
-	elVersion.style.color = "hsla(0,0%,100%,0.5)";
-	elVersion.style.textAlign = "right";
-	elVersion.textContent = "ver1";
-	
-	elMain.appendChild(elVersion);
-	elMain.appendChild(elInput);
-	elMain.appendChild(elDisplay);
-	elMain.appendChild(document.createTextNode("slider adjusts video playback rate"));
-	document.body.appendChild(elMain);
+	var controlEOKA = Object.keys(p.controlEO);
+	for (var controlEOKAI = 0,controlEOKAC = controlEOKA.length; controlEOKAI < controlEOKAC; controlEOKAI++){var controlEOK = controlEOKA[controlEOKAI];
+		var whatS = controlEOK;
+		var controlE = p.controlEO[whatS];
+		var elDisplayRow   = p.genElBase({typeS:"div"  ,classS:"funimationAssist-row"    ,elParent:elMain});
+		controlE.elDisplay = p.genElBase({typeS:"div"  ,classS:"funimationAssist-display",elParent:elDisplayRow});
+		controlE.elInput   = p.genElBase({typeS:"input",classS:"funimationAssist-input"  ,elParent:elDisplayRow});
+		controlE.elReset   = p.genElBase({typeS:"input",classS:"funimationAssist-reset"  ,elParent:elDisplayRow});
+		p.ooas(controlE.elInput,{type:"range",min:controlE.min,max:controlE.max,step:controlE.step,value:controlE.v});
+		p.ooas(controlE.elReset,{type:"button",value:"Reset"});
+		controlE.elInput.addEventListener("input",(function(whatS){return function(ev){p.set({whatS,valueM:this.value});};})(whatS));
+		controlE.elReset.addEventListener("click",(function(whatS){return function(ev){p.set({whatS,valueM:p.controlEO[whatS].reset});};})(whatS));
+		p.pipe({whatS});}
 })();
